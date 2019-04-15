a531cb073d9a731fbd1aca610ca8ff4e6d73fc12
Aarón <aaronarrieropelaez99@gmail.com>
2019-04-15 11:48:24 +0200

    Installed storybook, react-router and react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello, world
+        <Container>
+          <Row>
+            <Col>
+              Hello, world
+            </Col>
+            <Col>
+              Lorem ipsum
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
