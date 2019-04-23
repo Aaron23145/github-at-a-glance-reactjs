20878fef86e56d9a542577f87f0c5cb9ccded4c2
Aarón <aaronarrieropelaez99@gmail.com>
2019-04-23 10:02:53 +0200

    Finished Repository view

--- src/views/Repository/index.js
+++ src/views/Repository/index.js
@@ -43,18 +43,55 @@ class Repository extends Component {
 
     const repo = this.props.repository[this.fullName];
 
-    const { name, owner } = repo;
+    const {
+      name,
+      owner,
+      stargazers_count: stars,
+      subscribers_count: watchers,
+      open_issues_count: issues,
+      forks_count: forks,
+      language,
+      html_url,
+      description,
+    } = repo;
 
     return (
       <div className="Repository">
         <Row>
           <Col className="Repository__left-container">
             <Row>
-              <Col xs={12} className="Repository__name box">{ name }</Col>
+              <Col xs={12} className="Repository__name-lang box text-center">
+                <a href={html_url} target="_blank" rel="noreferrer noopener" className="Repository__name-lang__link">
+                  { name } ({ language }) <i className="fas fa-link" />
+                </a>
+              </Col>
               <Col xs={12} className="Repository__sw-container">
                 <Row>
-                  <Col className="Repository__stars box">Stars</Col>
-                  <Col className="Repository__watchers box">Watchers</Col>
+                  <Col className="Repository__stars box">
+                    <div className="d-flex justify-content-between align-items-center">
+                      { stars }
+                      <i className="fas fa-star" />
+                    </div>
+                  </Col>
+                  <Col className="Repository__watchers box">
+                    <div className="d-flex justify-content-between align-items-center">
+                      { watchers }
+                      <i className="fas fa-eye" />
+                    </div>
+                  </Col>
+                  <div className="w-100" />
+                  <Col className="Repository__issues box">
+                    <div className="d-flex justify-content-between align-items-center">
+                      { issues }
+                      <i className="fas fa-exclamation-circle" />
+                    </div>
+                  </Col>
+                  <Col className="Repository__forks box">
+                    <div className="d-flex justify-content-between align-items-center">
+                      { forks }
+                      <i className="fas fa-code-branch" />
+                    </div>
+                  </Col>
                 </Row>
               </Col>
             </Row>
@@ -63,6 +100,9 @@ class Repository extends Component {
             <AuthorContainer author={owner} />
           </Col>
         </Row>
+        <div className="Repository__description box">
+          { description }
+        </div>
       </div>
     );
   }
